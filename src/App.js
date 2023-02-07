c3e7b8eaf13fbbde66930c2899989c11b444808d
mhmd <mhmdemails@gmail.com>
2023-02-07 13:03:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>version 2</h1>
+      <h1 className="title">First React App</h1>
+      <h2> Mohamed Ali</h2>
+    </>
+  );
+}
+
+export default App;
